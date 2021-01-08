d869937c06389f06176de8324ab4d363916737a2
Kirstie Hayes <kirstiehayes@kirsties-mbp.home>
2021-01-08 18:23:00 +0000

    challenge one completed

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,18 +1,18 @@
-const removeTransition(e) {
+const removeTransition=(e)=> {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
   }
 
- const playSound(e) {
+ const playSound=(e)=> {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
+    if (!audio) return; //stop null being returned 
 
     key.classList.add('playing');
-    audio.currentTime = 0;
+    audio.currentTime = 0; ///rewind the sound to the start 
     audio.play();
   }
 
-  const keys =[...document.querySelectorAll('.key')];//new array
+  const keys =[...document.querySelectorAll('.keys')];//new array, could use .key or .keys div
   keys.map(key => key.addEventListener('transitionend', removeTransition));
   window.addEventListener('keydown', playSound);
